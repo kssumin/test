aebee0815a9b2b768352cb7f3d3cc2cb2bb1dc0f
kssumin <201566@jnu.ac.kr>
2024-08-01 19:50:51 +0900

    feat : template 코드

---
+++ src/main/java/webserver/WebServer.java
@@ -0,0 +1,32 @@
+package webserver;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WebServer {
+    private static final Logger log = LoggerFactory.getLogger(WebServer.class);
+    private static final int DEFAULT_PORT = 8080;
+
+    public static void main(String args[]) throws Exception {
+        int port = 0;
+        if (args == null || args.length == 0) {
+            port = DEFAULT_PORT;
+        } else {
+            port = Integer.parseInt(args[0]);
+        }
+
+        try (ServerSocket listenSocket = new ServerSocket(port)) {
+            log.info("Web Application Server started {} port.", port);
+
+            Socket connection;
+            while ((connection = listenSocket.accept()) != null) {
+                // 클라이언트와의 연결이 되면 클라이언트의 요청을 RequestHandler에 전달한다(새로운 스레드로)
+                RequestHandler requestHandler = new RequestHandler(connection);
+                requestHandler.start();
+            }
+        }
+    }
+}
